2677a63731cdd36ea7b667a5fb1503828afa809c
Marek Honzal <marek.honzal@applifting.cz>
2023-11-13 17:20:19 +0100

    feat: implement pwa configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,40 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import svgr from 'vite-plugin-svgr'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    svgr(),
+    VitePWA({
+      manifest: {
+        name: 'ZenToDo',
+        short_name: 'ZenToDo',
+        description: 'Installable ToDo app with Zen mode.',
+        start_url: '/',
+        orientation: 'portrait',
+        theme_color: '#EFEFEF',
+        scope: '/',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: 'pwa/icon-1024.png',
+            sizes: '1024x1024',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
 })
