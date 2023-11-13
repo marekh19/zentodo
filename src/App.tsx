9fdf56f9fa11930b58300ff382cde50865f5f790
Marek Honzal <marek.honzal@applifting.cz>
2023-11-13 12:57:16 +0100

    fix: double import mistake

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Header } from '@/components/Header'
 function App() {
   return (
     <main className="min-h-screen p-6">
-      <Header />
+      <Header className="mb-10" />
     </main>
   )
 }
