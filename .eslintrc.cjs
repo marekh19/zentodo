9cb4df2745fe9cd006bdf37cccd821367bf77c4e
Marek Honzal <marek.honzal@applifting.cz>
2023-11-13 12:07:16 +0100

    chore: setup code quality tools, tailwindcss

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:tailwindcss/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -13,6 +14,33 @@ module.exports = {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
+    ],
+    'tailwindcss/no-custom-classname': 'off',
+    'tailwindcss/classnames-order': 'off', // done via prettier plugin
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-restricted-imports': [
+      'error',
+      {
+        name: 'react',
+        importNames: ['default'],
+        message:
+          "Default React import is not necessary for JSX to work. Please use named imports. (e.g. `import { useEffect } from 'react'`) (https://reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html)",
+      },
+    ],
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        fixStyle: 'inline-type-imports',
+      },
     ],
   },
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
+/* eslint-disable no-restricted-imports */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import App from './App.tsx'
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
